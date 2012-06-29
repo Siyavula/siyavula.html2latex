f365db009cea81fe72de107f5ab0bfe28b4388f1
Ewald Zietsman <ewald@siyavula.org.za>
2012-06-29 14:22:26 +0200

    tweaks to build script for LS book

--- Grade10Lifescience/build_Gr10LS_pdf.py
+++ Grade10Lifescience/build_Gr10LS_pdf.py
@@ -16,35 +16,6 @@ from lxml import etree
 # the second level is chapters
 # chapters contain sections
 # each section is contained within in single html file
-class part:
-    def __init__(self, name, path):
-        self.chapters = []
-        self.name = name
-        self.path = path
-
-    def addSubPart(self, name, path):
-        self.chapters.append(chaptername)
-
-
-class book:
-    def __init__(self):
-        self.parts = []
-
-    def addPart(self, part):
-        '''partname is the folder name'''
-        self.parts.append(part)
-
-
-    def addSection(self, sectionname):
-        self.chapters.append(sectionname)
-
-#   def add(self, partname, depthname):
-#       if depthname == 'part':
-#           self.addPart(partname)
-#       elif depthname == 'chapter':
-#           self.addChapter(partname)
-#       elif depthname == 'section':
-#           self.add(
 
 def filename2chaptername(name):
     '''change the file-name-string into File name string'''
@@ -63,24 +34,68 @@ depthname = {0:'', 1:'part', 2:'chapter', 3:'section'}
 
 
 for dirpath, dirnames, filenames in os.walk('life-sciences-gr-10'):
-    print dirpath
+#    print dirpath
     depth = getdepth(dirpath)
     partname = dirpath.rpartition('/')[-1]
+
     if depth == 0:
-        print "only 1"
         section_file = open(dirpath+'/'+partname+'.tex','w')
     else:
         section_file.write('  '*(depth) + ' \\%s{%s}\n'%(depthname[depth], filename2chaptername(partname)))
 
+    print '  '*depth, partname
+    if filenames:
+        for f in filenames:
+            if ('.html' in f) and ('.swp' not in f):
+                filepath = os.path.join(dirpath,f)
+                print '   '*(depth+1), filepath
+                texfilename = filepath.replace('.html', '.tex')
+                section_file.write('  '*depth + '\\section{' + filename2chaptername(f.replace('.html', ''))+'}\n')
+                section_file.write('  '*depth + '\\input{%s}\n'%(filepath.partition('/')[2].replace('.html', '')))
 
-        # these are all sections contained in html files.
-        if depth == 2:
-            for f in filenames:
-                if '.html' in f:
-                    section_file.write('  '*depth + '\\section{' + filename2chaptername(f.replace('.html', ''))+'}\n')
-                    section_file.write('  '*depth + '\\input{' + '/'.join(dirpath.split('/')[1:])+'/'+f.replace('.html', '')+'}\n')
-                    #print "converting : ", dirpath+'/'+f
-                    os.system('../bin/python ../html2latex.py %s' % dirpath+'/'+f)
+                command = '../bin/python ../html2latex.py %s' % filepath
+                os.system(command)
+
+                # fix the image paths
+                try:
+                    texfile = open(texfilename, 'r')
+                    texcontents = texfile.read()
+                    texcontents = texcontents.replace(r'includegraphics[width=0.5\textwidth]{', r'includegraphics[width=0.5\textwidth]{%s'%(dirpath.partition('/')[2]+'/'))
+                    texfile.close()
+                    texfile = open(texfilename, 'w')
+                    texfile.write(texcontents)
+                    texfile.close()
+                except:
+                    print 'Something wrong with image fixing', texfilename
+                    pass
+#   if depth == 0:
+#       print "only 1"
+#       section_file = open(dirpath+'/'+partname+'.tex','w')
+#   else:
+#       section_file.write('  '*(depth) + ' \\%s{%s}\n'%(depthname[depth], filename2chaptername(partname)))
+
+
+#       # these are all sections contained in html files.
+#       if depth == 2:
+#           for f in filenames:
+#               if ('.html' in f) and ('.swp' not in f):
+#                   section_file.write('  '*depth + '\\section{' + filename2chaptername(f.replace('.html', ''))+'}\n')
+#                   section_file.write('  '*depth + '\\input{' + '/'.join(dirpath.split('/')[1:])+'/'+f.replace('.html', '')+'}\n')
+#                   #print "converting : ", dirpath+'/'+f
+#                   os.system('../bin/python ../html2latex.py %s' % dirpath+'/'+f)
+
+#                   # fix the image paths
+#                   try:
+#                       texfile = open(dirpath + '/' + f.replace('.html', '.tex'), 'r')
+#                       texcontents = texfile.read()
+#                       texcontents.replace('includegraphics[width=0.5\\textwidth]{', 'includegraphics[width=0.5\\textwidth]{%s'%(dirpath+'/'))
+#                       texfile.close()
+#                       texfile = open(dirpath + '/' + f, 'w')
+#                       texfile.write(texcontents)
+#                       texfile.close()
+#                   except:
+#                       pass
+                    
 
 
 #           section_file.write('\\chapter{%s}\n'%(filename2chaptername(partname)))
